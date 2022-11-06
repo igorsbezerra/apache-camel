4eea209a2d55a3bcc4f2221396f63f1bfb25b83f
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2022-11-06 10:44:12 -0300

    lendo json de um arquivo e enviado para fila no artemiscompose

---
+++ camel-microservice-a/src/main/java/com/example/camelmicroservicea/route/ActiveMqSenderRouter.java
@@ -0,0 +1,19 @@
+package com.example.camelmicroservicea.route;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ActiveMqSenderRouter extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+//        from("timer:active-mq-timer?period=10000")
+//                .transform().constant("My message for Active MQ")
+//                .log("${body}")
+//                .to("activemq:my-activemq-queue");
+
+        from("file:json")
+                .log("${body}")
+                .to("activemq:my-activemq-queue");
+    }
+}
---
+++ camel-microservices-b/src/main/java/com/example/camelmicroservicesb/route/ActiveMqReceiverRouter.java
@@ -0,0 +1,50 @@
+package com.example.camelmicroservicesb.route;
+
+import com.example.camelmicroservicesb.exchange.CurrencyExchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class ActiveMqReceiverRouter extends RouteBuilder {
+
+    private final MyCurrencyExchangeProcessor myCurrentExchangeProcessor;
+    private final MyCurrencyExchangeTransformer myCurrencyExchangeTransformer;
+
+    public ActiveMqReceiverRouter(MyCurrencyExchangeProcessor myCurrentExchangeProcessor, MyCurrencyExchangeTransformer myCurrencyExchangeTransformer) {
+        this.myCurrentExchangeProcessor = myCurrentExchangeProcessor;
+        this.myCurrencyExchangeTransformer = myCurrencyExchangeTransformer;
+    }
+
+    @Override
+    public void configure() throws Exception {
+        from("activemq:my-activemq-queue")
+                .unmarshal().json(JsonLibrary.Jackson, CurrencyExchange.class)
+                .bean(myCurrentExchangeProcessor)
+                .bean(myCurrencyExchangeTransformer)
+                .to("log:received-message-from-active-mq");
+    }
+}
+
+@Component
+class MyCurrencyExchangeProcessor {
+    private final Logger logger = LoggerFactory.getLogger(MyCurrencyExchangeProcessor.class);
+
+    public void processMessage(CurrencyExchange currencyExchange) {
+        logger.info("Do some processing with currencyExchange.getConversionMultiple() value which is {}",
+                currencyExchange.getConversionMultiple());
+    }
+}
+
+@Component
+class MyCurrencyExchangeTransformer {
+    public CurrencyExchange processMessage(CurrencyExchange currencyExchange) {
+        currencyExchange.setConversionMultiple(
+                currencyExchange.getConversionMultiple().multiply(BigDecimal.TEN));
+        return currencyExchange;
+    }
+}
